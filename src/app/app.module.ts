76b14326deb7e0e9a93cfa05daa4eb934fcacafa
Wellington Felix <wellington.felix@ufpe.br>
2017-10-20 09:18:37 -0300

    deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { MaterialModule } from './material.module';
-import { BacklogItemComponent } from './backlog-item/backlog-item.component';
 import { StatusFilterPipe } from './status-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BacklogItemComponent,
     StatusFilterPipe,
   ],
   imports: [
